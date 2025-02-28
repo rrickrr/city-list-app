af466620c166fa46cb1bfd87b39d79d9882f1229
Rick <rick@Elizabeths-MacBook-Air.local>
2025-02-27 21:04:15 -0500

    Initial commit - Vite React setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>City List App</h1>
+      <p>Let's build something awesome!</p>
+    </div>
+  );
+}
+
+export default App;
